5a7ef624276856dc71212f7c9a82ca14b85eb47e
lantingeee <lantingeee@gmail.com>
2017-09-29 23:58:22 +0800

    copy action modify

--- src/com/lt/copySqlPlugin/action/CopySqlSupportAction.java
+++ src/com/lt/copySqlPlugin/action/CopySqlSupportAction.java
@@ -15,11 +15,6 @@ public class CopySqlSupportAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent var1) {
-//        DataContext var2 = var1.getDataContext();
-//        CopyProvider var3 = PlatformDataKeys.COPY_PROVIDER.getData(var2);
-//        if (var3 != null) {
-//            var3.performCopy(var2);
-//        }
         String[] orgList = TransferSql.extractFromClip().split("\n|\r");
         String[] news = new CopySql().operateStrings(orgList);
         TransferSql.setContent(news);
